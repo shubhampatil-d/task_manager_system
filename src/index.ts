0d23e814922aba5039e2f5551f3c45ab623a423d
shubhampatil-d <shubhampatil21199@gmail.com>
2026-03-18 23:22:21 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(5000, () => {
+  console.log("Server running on 5000");
+});
